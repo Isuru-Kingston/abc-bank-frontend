ada5d9f2165515d3d8f127ef30a19ee4cee29a78
Ishan-Ravindu <ishanravindu27596@gmail.com>
2022-12-23 10:48:21 +0530

    added error handling for login form

---
+++ src/components/Login/LoginForm/index.jsx
@@ -0,0 +1,100 @@
+import { useState } from "react";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Typography from "@mui/material/Typography";
+import Alert from "@mui/material/Alert";
+import Avatar from "@mui/material/Avatar";
+import LoadingButton from "@mui/lab/LoadingButton";
+import TextField from "@mui/material/TextField";
+import Box from "@mui/material/Box";
+const Joi = require("joi");
+
+const schema = Joi.object({
+  userName: Joi.string().required(),
+  password: Joi.string().required(),
+});
+
+export default function LoginForm() {
+  const [userName, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(undefined);
+  const [isLording, setIsLording] = useState(false);
+
+  const handleSubmit = () => {
+    setIsLording(true);
+
+    const { value, error } = schema.validate({
+      userName,
+      password,
+    });
+
+    if (error) {
+      setError(error.details[0].message);
+      console.log(error);
+      setIsLording(false);
+    } else {
+      //api call here to login
+      console.log(value);
+      setTimeout(() => {
+        setError("server error");
+        setIsLording(false);
+      }, 2000);
+    }
+  };
+
+  const handleInputChange = (e, setInputState) => {
+    setError(undefined);
+    setInputState(e.target.value);
+  };
+
+  return (
+    <Box
+      sx={{
+        my: 8,
+        mx: 4,
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+      }}
+    >
+      <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+        <LockOutlinedIcon />
+      </Avatar>
+      <Typography component="h1" variant="h5">
+        Sign in
+      </Typography>
+      <Box sx={{ mt: 1 }}>
+        <TextField
+          onChange={(e) => handleInputChange(e, setUserName)}
+          margin="normal"
+          required
+          fullWidth
+          id="userName"
+          label="User Name"
+          name="userName"
+          autoFocus
+        />
+        <TextField
+          onChange={(e) => handleInputChange(e, setPassword)}
+          margin="normal"
+          required
+          fullWidth
+          name="password"
+          label="Password"
+          type="password"
+          id="password"
+          autoComplete="current-password"
+        />
+        <LoadingButton
+          loading={isLording}
+          onClick={() => handleSubmit()}
+          fullWidth
+          variant="contained"
+          sx={{ mt: 3, mb: 2 }}
+        >
+          Sign In
+        </LoadingButton>
+        {error && <Alert severity="error">{error}</Alert>}
+      </Box>
+    </Box>
+  );
+}
--- src/components/Login/index.jsx
+++ src/components/Login/index.jsx
@@ -1,119 +1,39 @@
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import CssBaseline from '@mui/material/CssBaseline';
-import TextField from '@mui/material/TextField';
-import Paper from '@mui/material/Paper';
-import Box from '@mui/material/Box';
-import Grid from '@mui/material/Grid';
-import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
-import Typography from '@mui/material/Typography';
-import Alert from '@mui/material/Alert';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-import CopyRight from '../CopyRight';
-import { useState } from 'react';
-const Joi = require('joi');
-
-const schema = Joi.object({
-    userName: Joi.string()
-        .required(),
-    password: Joi.string()
-        .required()
-})
+import CssBaseline from "@mui/material/CssBaseline";
+import Paper from "@mui/material/Paper";
+import Grid from "@mui/material/Grid";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import CopyRight from "../CopyRight";
+import LoginForm from "./LoginForm";
 
 const theme = createTheme();
 
-
-
 export default function Login() {
-    const [userName, setUserName] = useState('')
-    const [password, setPassword] = useState('')
-    const [error, setError] = useState()
-
-
-    const handleSubmit = () => {
-        const { value, error } = schema.validate({
-            userName,
-            password
-        });
-
-        if (error) {
-            setError(error)
-            console.log(error)
-        } else {
-            console.log(value);
-        }
-    };
-
-    return (
-        <ThemeProvider theme={theme}>
-            <Grid container component="main" sx={{ height: '100vh' }}>
-                <CssBaseline />
-                <Grid
-                    item
-                    xs={false}
-                    sm={4}
-                    md={7}
-                    sx={{
-                        // backgroundImage: 'url(https://source.unsplash.com/random)',
-                        backgroundRepeat: 'no-repeat',
-                        backgroundColor: (t) =>
-                            t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
-                        backgroundSize: 'cover',
-                        backgroundPosition: 'center',
-                    }}
-                />
-                <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
-                    <Box
-                        sx={{
-                            my: 8,
-                            mx: 4,
-                            display: 'flex',
-                            flexDirection: 'column',
-                            alignItems: 'center',
-                        }}
-                    >
-                        <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-                            <LockOutlinedIcon />
-                        </Avatar>
-                        <Typography component="h1" variant="h5">
-                            Sign in
-                        </Typography>
-                        <Box sx={{ mt: 1 }}>
-                            <TextField
-                                onChange={(e) => setUserName(e.target.value)}
-                                margin="normal"
-                                required
-                                fullWidth
-                                id="userName"
-                                label="User Name"
-                                name="userName"
-                                autoFocus
-                            />
-                            <TextField
-                                onChange={(e) => setPassword(e.target.value)}
-                                margin="normal"
-                                required
-                                fullWidth
-                                name="password"
-                                label="Password"
-                                type="password"
-                                id="password"
-                                autoComplete="current-password"
-                            />
-                            <Button
-                                onClick={() => handleSubmit()}
-                                fullWidth
-                                variant="contained"
-                                sx={{ mt: 3, mb: 2 }}
-                            >
-                                Sign In
-                            </Button>
-                            {error && <Alert severity="error">{error.ValidationError.name}</Alert>}
-                            <CopyRight sx={{ mt: 5 }} />
-                        </Box>
-                    </Box>
-                </Grid>
-            </Grid>
-        </ThemeProvider>
-    )
+  return (
+    <ThemeProvider theme={theme}>
+      <Grid container component="main" sx={{ height: "100vh" }}>
+        <CssBaseline />
+        <Grid
+          item
+          xs={false}
+          sm={4}
+          md={7}
+          sx={{
+            backgroundImage:
+              "url(https://images.unsplash.com/photo-1582139329536-e7284fece509?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1780&q=80)",
+            backgroundRepeat: "no-repeat",
+            backgroundColor: (t) =>
+              t.palette.mode === "light"
+                ? t.palette.grey[50]
+                : t.palette.grey[900],
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+          }}
+        />
+        <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+          <LoginForm />
+          <CopyRight sx={{ mt: 5 }} />
+        </Grid>
+      </Grid>
+    </ThemeProvider>
+  );
 }
