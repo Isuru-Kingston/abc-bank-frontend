48878d83594136d0adce9b4b4d119cb3f80cad9c
Ishan-Ravindu <ishanravindu27596@gmail.com>
2022-12-27 14:02:35 +0530

    added basic profile card to customer dashboard

--- src/components/Customer/index.jsx
+++ src/components/Customer/index.jsx
@@ -1,29 +1,13 @@
 import React, { useContext, useEffect, useState } from "react";
-import { styled } from "@mui/material/styles";
-import Grid from "@mui/material/Grid";
-import Paper from "@mui/material/Paper";
 import Box from "@mui/material/Box";
-import NavLink from "../NavBar/NavLink";
-import AccountBalanceIcon from "@mui/icons-material/AccountBalance";
-import PeopleIcon from "@mui/icons-material/People";
+import Paper from "@mui/material/Paper";
+import Typography from "@mui/material/Typography";
 import axios from "axios";
 
 import authContext from "../../context/authContext";
 
-const Item = styled(Paper)(({ theme }) => ({
-  backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
-  ...theme.typography.body2,
-  padding: theme.spacing(1),
-  textAlign: "center",
-  color: theme.palette.text.secondary,
-  display: "flex",
-  justifyContent: "center",
-  alignItems: "center",
-}));
-
 export default function Customer() {
   const [customer, setCustomer] = useState({});
-
   const authData = useContext(authContext);
 
   useEffect(() => {
@@ -50,35 +34,45 @@ export default function Customer() {
   };
 
   return (
+    //   {
+    //     "address": {
+    //         "street": "No 204, Muwapatigewela",
+    //         "postalCode": "32150",
+    //         "city": "Dehiaththakandiya",
+    //         "country": "Sri Lanka"
+    //     },
+    //     "_id": "63a7138fdf6af80f7b676284",
+    //     "user_id": "user_2",
+    //     "email": "isuru2@gmail.com",
+    //     "phone": "0756131317",
+    //     "nic": "982172462v",
+    //     "accounts": [
+    //         "63a7e103244cde1de3402691",
+    //         "63a7f04e28a7290f5d57272e"
+    //     ],
+    //     "is_active": true,
+    //     "createdAt": "2022-12-24T14:58:23.352Z",
+    //     "updatedAt": "2022-12-25T06:40:14.756Z",
+    //     "name": "Ishan Ravindu"
+    // }
     <Box
       sx={{
         width: "100%",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
       }}
     >
       <h1 style={{ textAlign: "center", marginBottom: "50px" }}>
-        Welcome To The Customer Dashboard !
+        Welcome Back {customer.name} !
       </h1>
-      {/* <Grid container rowSpacing={10} columnSpacing={{ xs: 1, sm: 2, md: 3 }}> */}
-      {/* <Grid item xs={6} style={{ marginTop: 10 }}>
-        <Item>
-          <NavLink
-            to={"/customer/account-list"}
-            name="Account List"
-            icon={<PeopleIcon />}
-          />
-        </Item>
-      </Grid>
-      <Grid item xs={6} style={{ marginTop: 10 }}>
-        <Item>
-          <NavLink
-            to={"/customer/transfer-money"}
-            name="Transfer Money"
-            icon={<AccountBalanceIcon />}
-          />
-        </Item>
-      </Grid> */}
-
-      {/* </Grid> */}
+      <Paper style={{ padding: "20px" }}>
+        <Typography variant="h5">name : {customer.name}</Typography>
+        <Typography variant="h5">email : {customer.email}</Typography>
+        <Typography variant="h5">phone :{customer.phone}</Typography>
+        <Typography variant="h5">NIC : {customer.nic}</Typography>
+      </Paper>
     </Box>
   );
 }
