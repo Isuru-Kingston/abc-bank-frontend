d1cd8ffc4c26d4d9e1b820b562fa779a93a9a888
Ishan-Ravindu <ishanravindu27596@gmail.com>
2022-12-27 23:20:19 +0530

    added api for add customer

--- src/components/Employee/CreateCustomer/index.jsx
+++ src/components/Employee/CreateCustomer/index.jsx
@@ -1,37 +1,51 @@
-import { useState } from "react";
 import AddIcon from "@mui/icons-material/Add";
-import Typography from "@mui/material/Typography";
+import LoadingButton from "@mui/lab/LoadingButton";
 import Alert from "@mui/material/Alert";
 import Avatar from "@mui/material/Avatar";
-import LoadingButton from "@mui/lab/LoadingButton";
-import TextField from "@mui/material/TextField";
 import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import Typography from "@mui/material/Typography";
+import axios from "axios";
+import { useContext, useState } from "react";
+import authContext from "../../../context/authContext";
 const Joi = require("joi");
 
 const schema = Joi.object({
   email: Joi.string().required(),
+  name: Joi.string().required(),
+  password: Joi.string().required(),
   phone: Joi.number().required(),
   street: Joi.string().required(),
   postalCode: Joi.number().required(),
   city: Joi.string().required(),
   country: Joi.string().required(),
-  possition: Joi.string().required(),
-  branch: Joi.string().required(),
   nic: Joi.string().required(),
 });
+// email,
+// name,
+// password,
+// phone,
+// street,
+// postalCode,
+// city,
+// country,
+// nic,
 
 export default function CreateCustomer() {
   const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const [phone, setphone] = useState("");
   const [street, setStreet] = useState("");
   const [postalCode, setPostalCode] = useState("");
   const [city, setcity] = useState("");
   const [country, setCountry] = useState("");
-  const [possition, setposition] = useState("");
-  const [branch, setBranch] = useState("");
+  const [name, setName] = useState("");
   const [nic, setNic] = useState("");
   const [error, setError] = useState(undefined);
   const [isLording, setIsLording] = useState(false);
+  const [isSuccess, setSuccess] = useState(false);
+
+  const authData = useContext(authContext);
 
   const handleSubmit = () => {
     setError(undefined);
@@ -39,13 +53,14 @@ export default function CreateCustomer() {
 
     const { value, error } = schema.validate({
       email,
+      name,
+      password,
       phone,
       street,
       postalCode,
       city,
       country,
-      possition,
-      branch,
+      name,
       nic,
     });
 
@@ -56,10 +71,45 @@ export default function CreateCustomer() {
     } else {
       //api call here to login
       console.log(value);
-      setTimeout(() => {
-        setError("server error");
-        setIsLording(false);
-      }, 2000);
+      axios
+        .post(
+          "http://localhost:8002/customer",
+          {
+            email,
+            name,
+            password,
+            phone,
+            street,
+            postalCode,
+            city,
+            country,
+            name,
+            nic,
+          },
+          {
+            headers: {
+              "Content-Type": "application/json",
+              "x-auth-token": authData.auth.token,
+            },
+          }
+        )
+        .then(function (response) {
+          setEmail("");
+          setName("");
+          setPassword("");
+          setphone("");
+          setStreet("");
+          setPostalCode("");
+          setcity("");
+          setNic("");
+          setCountry("");
+          setIsLording(false);
+          setSuccess(true);
+        })
+        .catch(function (error) {
+          setError(error.response.data.error);
+          setIsLording(false);
+        });
     }
   };
 
@@ -94,6 +144,28 @@ export default function CreateCustomer() {
           label="Email"
           name="email"
           autoFocus
+          value={email}
+        />
+        <TextField
+          onChange={(e) => handleInputChange(e, setPassword)}
+          margin="normal"
+          required
+          fullWidth
+          id="password"
+          label="password"
+          name="password"
+          autoFocus
+          value={password}
+        />
+        <TextField
+          onChange={(e) => handleInputChange(e, setName)}
+          margin="normal"
+          required
+          fullWidth
+          id="name"
+          label="Name"
+          name="name"
+          value={name}
         />
         <TextField
           onChange={(e) => handleInputChange(e, setphone)}
@@ -103,6 +175,7 @@ export default function CreateCustomer() {
           id="phone"
           label="Phone"
           name="phone"
+          value={phone}
         />
         <TextField
           onChange={(e) => handleInputChange(e, setStreet)}
@@ -112,6 +185,7 @@ export default function CreateCustomer() {
           id="street"
           label="Street"
           name="street"
+          value={street}
         />
         <TextField
           onChange={(e) => handleInputChange(e, setPostalCode)}
@@ -121,6 +195,7 @@ export default function CreateCustomer() {
           id="postalCode"
           label="PostalCode"
           name="postalCode"
+          value={postalCode}
         />
         <TextField
           onChange={(e) => handleInputChange(e, setcity)}
@@ -130,6 +205,7 @@ export default function CreateCustomer() {
           id="city"
           label="City"
           name="city"
+          value={city}
         />
         <TextField
           onChange={(e) => handleInputChange(e, setCountry)}
@@ -139,24 +215,7 @@ export default function CreateCustomer() {
           id="country"
           label="Country"
           name="country"
-        />
-        <TextField
-          onChange={(e) => handleInputChange(e, setposition)}
-          margin="normal"
-          required
-          fullWidth
-          id="possition"
-          label="Possition"
-          name="possition"
-        />
-        <TextField
-          onChange={(e) => handleInputChange(e, setBranch)}
-          margin="normal"
-          required
-          fullWidth
-          id="branch"
-          label="Branch"
-          name="branch"
+          value={country}
         />
         <TextField
           onChange={(e) => handleInputChange(e, setNic)}
@@ -166,6 +225,7 @@ export default function CreateCustomer() {
           id="nic"
           label="NIC"
           name="nic"
+          value={nic}
         />
         <LoadingButton
           loading={isLording}
